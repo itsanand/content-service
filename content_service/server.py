e21eacf0101e442c2a5788dcbe79fb559e042498
itsanand <cu.18bcs2174@gmail.com>
2023-10-15 22:31:22 +0530

    feat(swagger): added support for swagger doc

--- content_service/server.py
+++ content_service/server.py
@@ -2,8 +2,10 @@
 from starlette.applications import Starlette
 from starlette.routing import Route
 from content_service.endpoints.content_endpoint import ContentEndpoint
+from content_service.endpoints.swagger_doc import SwaggerDoc
 
 content_endpoint: ContentEndpoint = ContentEndpoint()
+swagger_doc: SwaggerDoc = SwaggerDoc()
 
 routes: list[Route] = [
     Route("/content", content_endpoint.create_content, methods=["POST"]),
@@ -12,6 +14,8 @@ routes: list[Route] = [
     Route("/content/{title}", content_endpoint.update_content, methods=["PATCH"]),
     Route("/content/{title}", content_endpoint.delete_content, methods=["DELETE"]),
     Route("/content/{title}", content_endpoint.fetch_content, methods=["GET"]),
+    Route("/docs", swagger_doc.swagger_ui, methods=["GET"]),
+    Route("/spec", swagger_doc.get_spec, methods=["GET"]),
 ]
 
 app: Starlette = Starlette(routes=routes)
