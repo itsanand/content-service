9ab8b33820f9531981bc484f63f29f1bb5021be3
itsanand <cu.18bcs2174@gmail.com>
2023-10-15 20:07:13 +0530

    feat(top-content): added support for top content api

--- content_service/server.py
+++ content_service/server.py
@@ -8,6 +8,7 @@ content_endpoint: ContentEndpoint = ContentEndpoint()
 routes: list[Route] = [
     Route("/content", content_endpoint.create_content, methods=["POST"]),
     Route("/content/new", content_endpoint.fetch_latest_content, methods=["GET"]),
+    Route("/content/top", content_endpoint.fetch_top_content, methods=["GET"]),
     Route("/content/{title}", content_endpoint.update_content, methods=["PATCH"]),
     Route("/content/{title}", content_endpoint.delete_content, methods=["DELETE"]),
     Route("/content/{title}", content_endpoint.fetch_content, methods=["GET"]),
